b13f63668a2375121b4df057dadad11d8b0f21c3
Kito Jack Beirens <kjackb1996@gmail.com>
2023-11-15 10:29:43 -0500

    change

--- app.js
+++ app.js
@@ -25,23 +25,22 @@ webSocket.onopen = () => {
     );
   };
 
-webSocket.onmessage = (event) => {
-    console.log(event);
+webSocket.onmessage = (event) => {    
     const data = JSON.parse(event.data)
+    console.log(data);
     if (data.type === "connection_ack") {
       webSocket.send(
         JSON.stringify(
           {
-            id: "my_id",
+            id: "my_subscription",
             type: "subscribe",
             payload: {
               "variables": {
-                "id": "0x28b3d0d4b39fefd3008500b57a1357a87573ada3:1",
-                "quoteToken": "token0"
+                "pairId": "0x839e2d728bfead41fc642a0ec4132b33ebb09d11:1"
               },
               "extensions": {},
-              "operationName": "CreateEvents",
-              "query": "subscription CreateEvents($id: String, $quoteToken: QuoteToken) {\n  onEventsCreated(id: $id, quoteToken: $quoteToken) {\n    address\n    id\n    networkId\n    events {\n      address\n      baseTokenPrice\n      blockNumber\n      eventDisplayType\n      eventType\n      id\n      liquidityToken\n      logIndex\n      maker\n      timestamp\n      token0ValueBase\n      token1ValueBase\n      transactionHash\n      transactionIndex\n      quoteToken\n      labels {\n        sandwich {\n          label\n          sandwichType\n          token0DrainedAmount\n          token1DrainedAmount\n          __typename\n        }\n        __typename\n      }\n      data {\n        __typename\n        ... on BurnEventData {\n          amount0\n          amount1\n          amount0Shifted\n          amount1Shifted\n          type\n          __typename\n        }\n        ... on MintEventData {\n          amount0\n          amount1\n          amount0Shifted\n          amount1Shifted\n          type\n          __typename\n        }\n        ... on SwapEventData {\n          amount0In\n          amount0Out\n          amount1In\n          amount1Out\n          amount0\n          amount1\n          amountNonLiquidityToken\n          priceUsd\n          priceUsdTotal\n          priceBaseToken\n          priceBaseTokenTotal\n          type\n          __typename\n        }\n      }\n      __typename\n    }\n    __typename\n  }\n}"
+              "operationName": "OnBarsUpdated",
+              "query": "subscription OnBarsUpdated($pairId: String!) { onBarsUpdated(pairId: $pairId) { pairAddress timestamp networkId } }", 
             }
           }
         )
